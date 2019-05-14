f3fa50892f68ac1a7a71e5b3d62a4de1064210b6
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2019-05-14 09:58:47 +0900

    sample spec complete

--- modules/usecases/src/main/scala/usecases/UseCase.scala
+++ modules/usecases/src/main/scala/usecases/UseCase.scala
@@ -2,6 +2,6 @@ package usecases
 
 trait UseCase[F[_], InputData, OutputData] {
 
-  def execute(inputData: InputData): F[OutputData]
+  def execute(inputData: InputData)(implicit ME: UseCaseMonadError[F]): F[OutputData]
 
 }
