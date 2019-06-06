85866677edb61bfeafb1b48c506d5592caa4a4cf
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2019-06-06 09:49:07 +0900

    implement findBy

--- modules/interfaces/src/main/scala/adapters/gateway/repositories/slick/AccountRepositoryBySlick.scala
+++ modules/interfaces/src/main/scala/adapters/gateway/repositories/slick/AccountRepositoryBySlick.scala
@@ -1,14 +1,29 @@
 package adapters.gateway.repositories.slick
 
 import adapters.Effect
-import domain.account.ResolvedAccount
 import domain.common.Email
 import scalaz.zio.ZIO
 import slick.jdbc.JdbcProfile
+import usecases.UseCaseSystemError
 
 class AccountRepositoryBySlick(override val profile: JdbcProfile, override val db: JdbcProfile#Backend#Database)
     extends AbstractAccountRepositoryBySlick(profile, db) {
 
-  override def findBy(email: Email): Effect[Option[ResolvedAccount]] = ZIO.succeed(None)
+  override def findBy(email: Email): Effect[Option[AggregateType]] =
+    for {
+      record <- ZIO
+        .fromFuture { implicit ec =>
+          import profile.api._
+          db.run(dao.filter(_.email === email.value.value).take(1).result)
+            .map(_.headOption)
+        }.foldM(
+          cause => ZIO.fail(UseCaseSystemError(cause)),
+          ZIO.succeed
+        )
+      aggregate <- record match {
+        case Some(r) => convertToAggregate(r).map(Some(_))
+        case None    => ZIO.succeed(None)
+      }
+    } yield aggregate
 
 }
--- modules/usecases/src/main/scala/repositories/AccountRepository.scala
+++ modules/usecases/src/main/scala/repositories/AccountRepository.scala
@@ -1,12 +1,12 @@
 package repositories
 
 import com.github.j5ik2o.dddbase.{ AggregateSingleReader, AggregateSingleWriter }
-import domain.account.{ Account, AccountId, ResolvedAccount }
+import domain.account.{ Account, AccountId }
 import domain.common.Email
 
 trait AccountRepository[F[_]] extends AggregateSingleReader[F] with AggregateSingleWriter[F] {
   override type AggregateType = Account
   override type IdType        = AccountId
 
-  def findBy(email: Email): F[Option[ResolvedAccount]]
+  def findBy(email: Email): F[Option[AggregateType]]
 }
