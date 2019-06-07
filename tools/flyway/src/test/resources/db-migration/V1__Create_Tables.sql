34899fa53d5c43b19bb9556f7984f4ff29953bf9
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2019-06-07 17:16:53 +0900

    add tools

--- tools/flyway/src/test/resources/db-migration/V1__Create_Tables.sql
+++ tools/flyway/src/test/resources/db-migration/V1__Create_Tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `account` (
   `id` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
